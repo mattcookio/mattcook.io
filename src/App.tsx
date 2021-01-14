73a399a44a514726d32b77cfbb442460defd21ac
Matt Cook <hello@mattcook.io>
2021-01-14 17:00:00 -0600

    Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'milligram/dist/milligram.min.css';
+import './App.sass';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      minHeight: '100vh',
+    }}>
+      <div>
+        <h1>matt cook</h1>
+        <h5>developer, designer, creative</h5>
+        <h6>currently working at <a href="https://aais.com">Ad Astra</a></h6>
+        <a href="mailto:hello@mattcook.io">hello@mattcook.io</a>
+      </div>
     </div>
   );
 }
