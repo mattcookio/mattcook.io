fb6600e8ed2c0e14ea02b9605c1f657f7d265b32
Matt Cook <mattcookkc@gmail.com>
2022-05-22 22:01:19 -0500

    Add link to startland article

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => {
         <h1>matt cook</h1>
         <h5><a href="mailto:hello@mattcook.io">hello@mattcook.io</a></h5>
         <h5>developer, designer, creative</h5>
-        <h5>cto and co-founder at Hively (site coming soon)</h5>
+        <h5>cto and co-founder at <a href="https://www.startlandnews.com/2021/09/ryan-townsend-hively-2/">Hively</a></h5>
         <h5>former software engineer @ <a href="https://www.equipmentshare.com/">EquipmentShare</a>, <a href="https://rfp360.com">RFP360</a>, <a href="https://www.aais.com">Ad Astra</a></h5>
       </div>
     </div>
